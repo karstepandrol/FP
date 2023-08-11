e7387b1911bd7258009b927d8b59028208f36345
Karstepandrol <andy-man1@yandex.ru>
2023-08-11 16:28:32 +0300

    9 exercise

--- src/ru/progwards/java1/lessons/basics/Distance3D.java
+++ src/ru/progwards/java1/lessons/basics/Distance3D.java
@@ -1,21 +1,21 @@
 package ru.progwards.java1.lessons.basics;
 
 public class Distance3D {
-    public static double distance(double x1, double y1,double z1, double x2, double y2,
-                                  double z2) {
+    public static double distance(double x1, double y1,double z1, double x2, double y2, double z2) {
         double AB;
+        double CD;
         double xb = x2;
         double xa = x1;
         double yb = y2;
         double ya = y1;
         double zb = z2;
         double za = z1;
-        AB = (xb - xa)*2 + (yb - ya)*2 + (zb - za)*2;
+        AB = Math.pow((xb - xa), 2) + Math.pow((yb - ya), 2) + Math.pow((zb - za), 2);
+        CD = Math.sqrt(AB);
         System.out.println(Math.sqrt(AB));
-        return AB;
+        return CD;
     }
-
     public static void main(String[] args) {
-  //      distance(36.265195026061036, 24.464059817226385, 35.35018883007724, 24.464059817226385, 24.464059817226385, 33.17537824627905);
+        distance(1, 1, 1, 1, 4, 5);
     }
 }
--- src/ru/progwards/java1/lessons/basics/ReverseDigits.java
+++ src/ru/progwards/java1/lessons/basics/ReverseDigits.java
@@ -2,18 +2,14 @@ package ru.progwards.java1.lessons.basics;
 
 public class ReverseDigits {
     public static int reverseDigits(int number) {
-        int A = number % 10;
-        int B =(number % 100 - number % 10) / 10;
+        int A = number % 10 * 100;
+        int B = (number % 100 - number % 10);
         int C = (number - number % 100) / 100;
-        System.out.print(number % 10);
-        System.out.print((number % 100 - number % 10) / 10);
-        System.out.println((number - number % 100) / 100);
+        System.out.println(number);
+        System.out.print(A + B + C);
         return A + B + C;
     }
     public static void main(String[] args) {
-     //   reverseDigits(123);
-     //   reverseDigits(456);
-     //   reverseDigits(789);
-     //   reverseDigits(910);
+        reverseDigits(123);
     }
 }
